daefefd520f6ca28b09abd3e69673d0924647bb9
Olin Blodgett <nilo@nilosplace.net>
2015-11-10 14:26:36 -0500

    Added test cases

--- src/test/java/org/jax/mgi/snpindexer/MainTest.java
+++ src/test/java/org/jax/mgi/snpindexer/MainTest.java
@@ -1,9 +1,14 @@
 package org.jax.mgi.snpindexer;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 import org.jax.mgi.snpdatamodel.ConsensusSNP;
 import org.jax.mgi.snpdatamodel.visitors.PrintVisitor;
 import org.jax.mgi.snpindexer.indexes.ConsensusSNPIndexer;
@@ -14,20 +19,25 @@ import org.jax.mgi.snpindexer.util.SQLExecutor;
 
 public class MainTest {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		ConfigurationHelper.init();
 		
-//		ConsensusSNPIndexer i = new ConsensusSNPIndexer("ConsensusSNPIndex");
-//		
-//		HashMap<Integer, ConsensusSNP> consensusList = i.getConsensusSNP(2, 4);
-//
-//		for(ConsensusSNP c: consensusList.values()) {
-//			if(c.getConsensusKey() == 3) {
-//				PrintVisitor pi = new PrintVisitor();
-//				c.Accept(pi);
-//				pi.generateOutput(System.out);
-//			}
-//		}
+		ConsensusSNPIndexer i = new ConsensusSNPIndexer("ConsensusSNPIndex");
+		
+		HashMap<Integer, ConsensusSNP> consensusList = i.getConsensusSNP(2, 4);
+
+		for(ConsensusSNP c: consensusList.values()) {
+			if(c.getConsensusKey() == 3) {
+				PrintVisitor pi = new PrintVisitor();
+				c.Accept(pi);
+				pi.generateOutput(System.out);
+				
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.setSerializationInclusion(Inclusion.NON_NULL);
+				String json = mapper.writeValueAsString(c);
+				System.out.println("Json: " + json);
+			}
+		}
 		
 //		SQLExecutor sql = new SQLExecutor(50000, false);
 //		HashMap<Integer, String> hash = null;
