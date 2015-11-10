e6def7db4cdc3804610f13a40e5540b04ba619a1
Olin Blodgett <nilo@nilosplace.net>
2015-11-10 13:09:27 -0500

    Added some sql testing

--- src/test/java/org/jax/mgi/snpindexer/MainTest.java
+++ src/test/java/org/jax/mgi/snpindexer/MainTest.java
@@ -1,5 +1,6 @@
 package org.jax.mgi.snpindexer;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -7,6 +8,7 @@ import org.jax.mgi.snpdatamodel.ConsensusSNP;
 import org.jax.mgi.snpdatamodel.visitors.PrintVisitor;
 import org.jax.mgi.snpindexer.indexes.ConsensusSNPIndexer;
 import org.jax.mgi.snpindexer.util.ConfigurationHelper;
+import org.jax.mgi.snpindexer.util.SQLExecutor;
 
 
 
@@ -15,17 +17,37 @@ public class MainTest {
 	public static void main(String[] args) throws SQLException {
 		ConfigurationHelper.init();
 		
-		ConsensusSNPIndexer i = new ConsensusSNPIndexer("ConsensusSNPIndex");
+//		ConsensusSNPIndexer i = new ConsensusSNPIndexer("ConsensusSNPIndex");
+//		
+//		HashMap<Integer, ConsensusSNP> consensusList = i.getConsensusSNP(2, 4);
+//
+//		for(ConsensusSNP c: consensusList.values()) {
+//			if(c.getConsensusKey() == 3) {
+//				PrintVisitor pi = new PrintVisitor();
+//				c.Accept(pi);
+//				pi.generateOutput(System.out);
+//			}
+//		}
 		
-		HashMap<Integer, ConsensusSNP> consensusList = i.getConsensusSNP(2, 4);
-
-		for(ConsensusSNP c: consensusList.values()) {
-			if(c.getConsensusKey() == 3) {
-				PrintVisitor pi = new PrintVisitor();
-				c.Accept(pi);
-				pi.generateOutput(System.out);
-			}
-		}
+//		SQLExecutor sql = new SQLExecutor(50000, false);
+//		HashMap<Integer, String> hash = null;
+//		
+//		if(hash == null) {
+//			hash = new HashMap<Integer, String>();
+//			
+//			// Cache the strings so submitters doesn't blow out memory
+//			HashMap<String, String> temp = new HashMap<String, String>();
+//			
+//			ResultSet set = sql.executeQuery("select _object_key, accid from snp.snp_accession where _logicaldb_key = 74 and _mgitype_key = 31");
+//			while(set.next()) {
+//				if(!temp.containsKey(set.getString("accid"))) {
+//					temp.put(set.getString("accid"), set.getString("accid"));
+//				}
+//				System.out.println(temp.size());
+//				hash.put(set.getInt("_object_key"), temp.get(set.getString("accid")));
+//			}
+//			set.close();
+//		}
 		
 	}
 }
