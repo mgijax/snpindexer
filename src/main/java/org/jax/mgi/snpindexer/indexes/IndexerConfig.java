522c458cf9bcb1d3eec7e7568b046b49523469ce
Olin Blodgett <nilo@nilosplace.net>
2015-11-25 07:56:24 -0500

    Change the chunk size for search indexer

--- src/main/java/org/jax/mgi/snpindexer/indexes/IndexerConfig.java
+++ src/main/java/org/jax/mgi/snpindexer/indexes/IndexerConfig.java
@@ -5,7 +5,7 @@ public enum IndexerConfig {
 	// (SolrCoreName, IndexerClass, ChunkSize, CommitTimeout, DB Fetch Size)
 	
 	ConsensusSNPIndexer("ConsensusSNPIndex", ConsensusSNPIndexer.class, 25000, 120000, 50000),
-	SearchSNPIndexer("SearchSNPIndex", SearchSNPIndexer.class, 10000, 30000, 50000)
+	SearchSNPIndexer("SearchSNPIndex", SearchSNPIndexer.class, 4000, 30000, 50000)
 	;
 	
 	// Batch size to process from the database
