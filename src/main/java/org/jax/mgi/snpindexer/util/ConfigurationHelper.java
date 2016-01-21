a74d92106d54e942694ecf8d33d85e7ae89cd719
Olin Blodgett <nilo@nilosplace.net>
2016-01-21 09:35:49 -0500

    Added more debuging for configuration output

--- src/main/java/org/jax/mgi/snpindexer/util/ConfigurationHelper.java
+++ src/main/java/org/jax/mgi/snpindexer/util/ConfigurationHelper.java
@@ -30,14 +30,27 @@ public class ConfigurationHelper {
 		
 		log = Logger.getLogger(ConfigurationHelper.class);
 		log.info("Loading System Properties via -D paramaters");
+		
 		driver = System.getProperty("PG_DBDRIVER");
+		if(driver != null) { log.info("Found: -D PG_DBDRIVER=" + driver); }
+		
 		databaseUrl = System.getProperty("PG_DBURL");
+		if(databaseUrl != null) { log.info("Found: -D PG_DBURL=" + databaseUrl); }
+		
 		user = System.getProperty("PG_DBUSER");
+		if(user != null) { log.info("Found: -D PG_DBUSER=" + user); }
+		
 		password = System.getProperty("PG_DBPASS");
+		if(password != null) { log.info("Found: -D PG_DBPASS=" + password); }
+		
 		solrBaseUrl = System.getProperty("SOLR_BASEURL");
+		if(solrBaseUrl != null) { log.info("Found: -D SOLR_BASEURL=" + solrBaseUrl); }
+		
 		debug = "true".equals(System.getProperty("DEBUG"));
+		if(debug) { log.info("Found: -D DEBUG=" + debug); }
+		
 		threaded = "true".equals(System.getProperty("THREADED"));
-		printProperties();
+		if(threaded) { log.info("Found: -D THREADED=" + threaded); }
 		
 		log.info("Loading Properties via config.properties files");
 		in = SQLExecutor.class.getClassLoader().getResourceAsStream("config.properties");
@@ -47,42 +60,98 @@ public class ConfigurationHelper {
 			try {
 				Properties configurationProperties = new Properties();
 				configurationProperties.load(in);
-				if(driver == null) driver = configurationProperties.getProperty("driver");
-				if(databaseUrl == null) databaseUrl = configurationProperties.getProperty("databaseUrl");
-				if(user == null) user = configurationProperties.getProperty("user");
-				if(password == null) password = configurationProperties.getProperty("password");
-				if(solrBaseUrl == null) solrBaseUrl = configurationProperties.getProperty("solrBaseUrl");
-				if(!debug) debug = "true".equals(configurationProperties.getProperty("debug"));
-				if(!threaded) threaded = "true".equals(configurationProperties.getProperty("threaded"));
+				if(driver == null) {
+					driver = configurationProperties.getProperty("driver");
+					if(driver != null) { log.info("Config File: driver=" + driver); }
+				}
+				if(databaseUrl == null) {
+					databaseUrl = configurationProperties.getProperty("databaseUrl");
+					if(databaseUrl != null) { log.info("Config File: databaseUrl=" + databaseUrl); }
+				}
+				if(user == null) {
+					user = configurationProperties.getProperty("user");
+					if(user != null) { log.info("Config File: user=" + user); }
+				}
+				if(password == null) {
+					password = configurationProperties.getProperty("password");
+					if(password != null) { log.info("Config File: password=" + password); }
+				}
+				if(solrBaseUrl == null) {
+					solrBaseUrl = configurationProperties.getProperty("solrBaseUrl");
+					if(solrBaseUrl != null) { log.info("Config File: solrBaseUrl=" + solrBaseUrl); }
+				}
+				if(!debug) {
+					debug = "true".equals(configurationProperties.getProperty("debug"));
+					if(debug) { log.info("Config File: debug=" + debug); }
+				}
+				if(!threaded) {
+					threaded = "true".equals(configurationProperties.getProperty("threaded"));
+					if(threaded) { log.info("Config File: threaded=" + threaded); }
+				}
 			} catch (IOException e) {
 				log.info("Error: " + e.getMessage());
 				log.info("Error Loading config.properties file assuming defaults");
 			}
 		}
-		printProperties();
 		
 		log.info("Loading Properties via System ENV");
-		if(driver == null) driver = System.getenv("PG_DBDRIVER");
-		if(databaseUrl == null) databaseUrl = System.getenv("PG_DBURL");
-		if(user == null) user = System.getenv("PG_DBUSER");
-		if(password == null) password = System.getenv("PG_DBPASS");
-		if(solrBaseUrl == null) solrBaseUrl = System.getenv("SOLR_BASEURL");
-		if(!debug) debug = "true".equals(System.getenv("DEBUG"));
-		if(!threaded) threaded = "true".equals(System.getenv("THREADED"));
-		printProperties();
+		if(driver == null) {
+			driver = System.getenv("PG_DBDRIVER");
+			if(driver != null) { log.info("Found Enviroment ENV[PG_DBDRIVER]=" + driver); }
+		}
+		if(databaseUrl == null) {
+			databaseUrl = System.getenv("PG_DBURL");
+			if(databaseUrl != null) { log.info("Found Enviroment ENV[PG_DBURL]=" + databaseUrl); }
+		}
+		if(user == null) {
+			user = System.getenv("PG_DBUSER");
+			if(user != null) { log.info("Found Enviroment ENV[PG_DBUSER]=" + user); }
+		}
+		if(password == null) {
+			password = System.getenv("PG_DBPASS");
+			if(password != null) { log.info("Found Enviroment ENV[PG_DBPASS]=" + password); }
+		}
+		if(solrBaseUrl == null) {
+			solrBaseUrl = System.getenv("SOLR_BASEURL");
+			if(solrBaseUrl != null) { log.info("Found Enviroment ENV[SOLR_BASEURL]=" + solrBaseUrl); }
+		}
+		if(!debug) {
+			debug = "true".equals(System.getenv("DEBUG"));
+			if(debug) { log.info("Found Enviroment ENV[DEBUG]=" + debug); }
+		}
+		if(!threaded) {
+			threaded = "true".equals(System.getenv("THREADED"));
+			if(threaded) { log.info("Found Enviroment ENV[THREADED]=" + threaded); }
+		}
 		
 		log.info("Setting default values for properties that are null");
-		if(driver == null) driver = "org.postgresql.Driver";
-		if(databaseUrl == null) databaseUrl = "jdbc:postgresql://localhost/export";
-		if(user == null) user = "mgd_public";
-		if(password == null) password = "mgdpub";
-		if(solrBaseUrl == null) solrBaseUrl = "http://localhost.jax.org:8983/solr";
+		if(driver == null) {
+			driver = "org.postgresql.Driver";
+			log.info("Setting default: driver=" + driver);
+		}
+		if(databaseUrl == null) {
+			databaseUrl = "jdbc:postgresql://localhost/export";
+			log.info("Setting default: databaseUrl=" + databaseUrl);
+		}
+		if(user == null) {
+			user = "mgd_public";
+			log.info("Setting default: user=" + user);
+		}
+		if(password == null) {
+			password = "mgdpub";
+			log.info("Setting default: password=" + password);
+		}
+		if(solrBaseUrl == null) {
+			solrBaseUrl = "http://localhost.jax.org:8983/solr";
+			log.info("Setting default: solrBaseUrl=" + solrBaseUrl);
+		}
+		
 		printProperties();
 		
 	}
 	
 	public static void printProperties() {
-		log.info("Effective Properties:");
+		log.info("Running Indexer with Properties:");
 		log.info("\tdriver: " + driver);
 		log.info("\tdatabaseUrl: " + databaseUrl);
 		log.info("\tuser: " + user);
